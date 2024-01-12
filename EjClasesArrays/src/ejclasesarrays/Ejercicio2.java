bfd7abff0a713c8a92eba45c7066679e6ed03a73
jlbarrionuevo <jlbarrionuevo@106-06.info2>
2024-01-12 14:35:57 +0100

    Ejercicios

---
+++ EjClasesArrays/src/ejclasesarrays/Ejercicio2.java
@@ -0,0 +1,21 @@
+package ejclasesarrays;
+
+import java.util.Arrays;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		int tabla[] = new int[55];
+		
+		
+
+		/*Arrays.fill(tabla, 0, 1, 1);
+		Arrays.fill(tabla, 1, 3, 2);
+		Arrays.fill(tabla, 3, 6, 3);
+		Arrays.fill(tabla, )*/
+		
+		System.out.println(Arrays.toString(tabla));
+	}
+
+}
