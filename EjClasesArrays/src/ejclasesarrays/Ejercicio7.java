27ebc3f5bfc1c925d5283eea2cf5cf8b9f0bea0a
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-01-16 19:59:14 +0100

    Ejercicios

--- EjClasesArrays/src/ejclasesarrays/Ejercicio7.java
+++ EjClasesArrays/src/ejclasesarrays/Ejercicio7.java
@@ -6,18 +6,38 @@ public class Ejercicio7 {
 
 	public static void main(String[] args) {
 
-		int aleatorio;
+		int aleatorio = 0;
 
-		int primitiva[] = new int[6];
+		int apuesta[] = new int[6];
 
-		int apuesta[] = {9, 21, 37, 38, 41, 49};
+		int ganadora[] = { 9, 21, 37, 38, 41, 49 };
 
-		for (int i = 0; i < primitiva.length; i++) {
+		for (int i = 0; i < apuesta.length; i++) {
 			aleatorio = (int) (1 + Math.random() * 49);
-			primitiva[i] = aleatorio;
+			apuesta[i] = aleatorio;
 		}
 
-		System.out.println(Arrays.toString(primitiva));
+		int contador = 0;
+
+		Arrays.sort(apuesta);
+
+		System.out.println("Apuesta -->       " + Arrays.toString(apuesta));
+		System.out.println("Combinación ganadora... ");
+		System.out.println("");
+		System.out.println("Serie ganadora --> " + Arrays.toString(ganadora));
+
+		for (int i = 0; i < apuesta.length; i++) {
+			// Utilizamos la clase binarySearch para buscar el número en la combinación
+			// ganadora
+			int indice = Arrays.binarySearch(ganadora, apuesta[i]);
+
+			// Si el índice es mayor o igual a 0, significa que encontramos un número.
+			if (indice >= 0) {
+				contador++;
+			}
+		}
+
+		System.out.println("Número de aciertos: " + contador);
 
 	}
 
